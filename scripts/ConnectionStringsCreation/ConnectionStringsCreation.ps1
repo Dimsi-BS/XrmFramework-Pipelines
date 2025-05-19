ee89c11c19cc4a9c2570e3a030cd9c2f66edb169
Aymeric Schneider <a.schneider-maunoury@dimsi.fr>
2025-05-19 16:25:01 +0200

    [ Standalone ] Upgrade Pipe

--- scripts/ConnectionStringsCreation/ConnectionStringsCreation.ps1
+++ scripts/ConnectionStringsCreation/ConnectionStringsCreation.ps1
@@ -4,16 +4,16 @@ param()
 . "$PSScriptRoot\PowerAppsAdminUtilities.ps1"
 Import-Module "$PSScriptRoot\ps_modules\VstsTaskSdk\VstsTaskSdk.psm1"
 
-Trace-VstsEnteringInvocation $MyInvocation
+Trace-EnteringInvocation $MyInvocation
 
 # Get inputs for the task
-$authenticationType = Get-VstsInput -Name authenticationType -Require
-$connectedServiceName = Get-VstsInput -Name $authenticationType -Require
+$authenticationType = Get-Input -Name authenticationType -Require
+$connectedServiceName = Get-Input -Name $authenticationType -Require
 
-$xrmFrameworkConfigPath = Get-VstsInput -Name XrmFrameworkConfigPath -Require
-$connectionStringsConfigPath = Get-VstsInput -Name ConnectionStringsConfigPath -Require
+$xrmFrameworkConfigPath = Get-Input -Name XrmFrameworkConfigPath -Require
+$connectionStringsConfigPath = Get-Input -Name ConnectionStringsConfigPath -Require
 
-$endpoint = Get-VstsEndpoint -Name $connectedServiceName -Require
+$endpoint = Get-Endpoint -Name $connectedServiceName -Require
 
 $connectionString = Get-ConnectionString -Endpoint $endpoint -AuthenticationType $authenticationType
 
